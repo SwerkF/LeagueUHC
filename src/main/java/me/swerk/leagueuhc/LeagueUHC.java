1daf5a13b3bfebb63c2af1e3783217791ffadb0a
Swerk <swerk.pro@gmail.com>
2022-05-04 00:16:32 +0200

    Plugin instance
    Adding plugin instance

--- src/main/java/me/swerk/leagueuhc/LeagueUHC.java
+++ src/main/java/me/swerk/leagueuhc/LeagueUHC.java
@@ -1,21 +1,23 @@
 package me.swerk.leagueuhc;
 
-import me.swerk.leagueuhc.commands.backPackCommand;
-import me.swerk.leagueuhc.commands.feedCommand;
-import me.swerk.leagueuhc.commands.gm0Command;
-import me.swerk.leagueuhc.commands.gm1Command;
+import me.swerk.leagueuhc.commands.*;
 import me.swerk.leagueuhc.listeners.JoinLeaveListener;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+
+
 public final class LeagueUHC extends JavaPlugin implements Listener {
 
+    private static LeagueUHC plugin;
     @Override
     public void onEnable() {
         // Plugin startup logic
+
+        plugin = this;
+
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
 
         System.out.println("Le plugin a démarré...");
         getServer().getPluginManager().registerEvents(new JoinLeaveListener(), this );
@@ -23,11 +25,10 @@ public final class LeagueUHC extends JavaPlugin implements Listener {
         getCommand("gm1").setExecutor(new gm1Command());
         getCommand("gm0").setExecutor(new gm0Command());
         getCommand("backpack").setExecutor(new backPackCommand());
+        getCommand("repeat").setExecutor(new repeatCommand());
     }
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        return super.onCommand(sender, command, label, args);
+    public static LeagueUHC getPlugin() {
+        return plugin;
     }
-
 }
